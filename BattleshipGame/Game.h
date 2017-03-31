fcce4c83db8fbba4b22a5a619a93f9a666235e24
LiadW <liadwg2@gmail.com>
2017-03-31 14:12:43 +0300

    implemented battleBoard.h

--- BattleshipGame/Game.h
+++ BattleshipGame/Game.h
@@ -7,8 +7,4 @@
 using namespace std;
 
 bool CheckValidPath(string gameFolder);
-int CalcScore(char** board);
-string CheckVictory(char** board);
-bool isBoardValid(char** board);
-char** readBoard(string boardPath);
 vector<string> getGameFiles(string folder);
