736bf5f5212c5f6cda8926c62eabb23cd967b9c4
shiranmazor <shiran.mazpr@gmail.com>
2017-03-31 14:19:59 +0300

    zx

--- BattleshipGame/Game.h
+++ BattleshipGame/Game.h
@@ -1,14 +1,10 @@
 #pragma once
 #include <iostream>
 #include <cstring>
-#include <windows.h>
+#include <direct.h>
 #include <vector>
+
 using namespace std;
 
-
 bool CheckValidPath(string gameFolder);
-int CalcScore(char** board);
-string CheckVictory(char** board);
-bool isBoardValid(char** board);
-char** readBoard(string boardPath);
-void getGameFiles(string folder, vector<string> & gameFiles);
+vector<string> getGameFiles(string folder);
