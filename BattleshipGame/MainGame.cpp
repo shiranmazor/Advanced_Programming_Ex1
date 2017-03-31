bf4645a75354ef6e9f4b5f5a913389ab0f8be263
shiranmazor <shiran.mazpr@gmail.com>
2017-03-31 12:23:52 +0300

    new files

--- BattleshipGame/MainGame.cpp
+++ BattleshipGame/MainGame.cpp
@@ -1,7 +1,22 @@
 #include "BattleshipGameAlgo.h"
+#include "Game.h"
+#include <algorithm>
+
+vector<string> getGameFiles(string folder)
+{
+	
+}
+
 
 
 int main(int argc, char **argv)
 {
-	
+	if (argc < 2)
+	{
+		//search files in working directory
+	}
+	else
+	{
+		
+	}
 }
