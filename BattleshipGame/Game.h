28480d344357e2e9daa17ff6719b620856976b11
shiranmazor <shiran.mazpr@gmail.com>
2017-03-31 14:16:30 +0300

    sfsd

--- BattleshipGame/Game.h
+++ BattleshipGame/Game.h
@@ -1,14 +1,14 @@
 #pragma once
 #include <iostream>
 #include <cstring>
-#include <direct.h>
+#include <windows.h>
 #include <vector>
-
 using namespace std;
+
 
 bool CheckValidPath(string gameFolder);
 int CalcScore(char** board);
 string CheckVictory(char** board);
 bool isBoardValid(char** board);
 char** readBoard(string boardPath);
-vector<string> getGameFiles(string folder);
+void getGameFiles(string folder, vector<string> & gameFiles);
--- BattleshipGame/MainGame.cpp
+++ BattleshipGame/MainGame.cpp
@@ -2,11 +2,67 @@
 #include "Game.h"
 #include <algorithm>
 
-vector<string> getGameFiles(string folder)
+void getGameFiles(string folder, vector<string> & gameFiles)
 {
-	
-}
+	WIN32_FIND_DATA search_data;
+	std::wstring stemp;
+	HANDLE handle;
+	memset(&search_data, 0, sizeof(WIN32_FIND_DATA));
 
+	//start with sboard files
+	string sboardPath = folder + "\\*.sboard";
+	stemp = std::wstring(sboardPath.begin(), sboardPath.end());
+	LPCWSTR bPath = stemp.c_str();
+	handle = FindFirstFile(bPath, &search_data);
+
+	if (handle != INVALID_HANDLE_VALUE)
+	{
+		//check file extension
+		wstring ws(search_data.cFileName);
+		string filename(ws.begin(), ws.end());
+
+		if (filename.find("sboard") != std::string::npos)
+		{
+			//found sboard file
+			string fullPath = folder + "\\" + filename;
+			gameFiles.push_back(fullPath);
+		}
+	}
+
+	//start with attack-a files
+	string attackAPath = folder + "\\*.attack-a";
+	stemp = std::wstring(attackAPath.begin(), attackAPath.end());
+	LPCWSTR attackPathA = stemp.c_str();
+	handle = FindFirstFile(attackPathA, &search_data);
+
+	if (handle != INVALID_HANDLE_VALUE)
+	{
+		//check file extension
+		wstring ws(search_data.cFileName);
+		string filename(ws.begin(), ws.end());
+
+		if (filename.find("attack-a") != std::string::npos)
+		{
+			//found sboard file
+			string fullPath = folder + "\\" + filename;
+			gameFiles.push_back(fullPath);
+		}
+	}
+
+}
+void getFilesList(string filePath, string extension, vector<string> & returnFileName)
+{
+	WIN32_FIND_DATA fileInfo;
+	HANDLE hFind;
+	string  fullPath = filePath + extension;
+	hFind = FindFirstFile(fullPath.c_str(), &fileInfo);
+	if (hFind != INVALID_HANDLE_VALUE) {
+		returnFileName.push_back(filePath + fileInfo.cFileName);
+		while (FindNextFile(hFind, &fileInfo) != 0) {
+			returnFileName.push_back(filePath + fileInfo.cFileName);
+		}
+	}
+}
 
 
 int main(int argc, char **argv)
@@ -19,4 +75,6 @@ int main(int argc, char **argv)
 	{
 		
 	}
+	
+
 }
